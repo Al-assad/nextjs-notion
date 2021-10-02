c0aa7ae9370c7f9a2f4f0173e22629aa98ea2afd
al-assad <yulin.ying@outlook.com>
2021-10-02 20:25:30 +0800

    update

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '2d04652a413e4beaaa109b4eed2f6fa6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Al-assad\'s Chaos',
+  domain: 'al-assad.github.io',
+  author: 'Al-assad',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'Al-assad\'s Chaos',
+  socialImageTitle: 'Al-assad',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Alassad_dev',
+  github: 'Al-assad',
+  telegram: 'alassad_dev',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
